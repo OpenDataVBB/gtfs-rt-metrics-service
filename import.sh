560f7b6be247d51e31141bf3fd8c78dd87dcedb2
Jannis R <mail@jannisr.de>
2026-02-09 12:03:43 +0000

    import script: make duckdb-gtfs-importer options configurable

--- import.sh
+++ import.sh
@@ -2,16 +2,21 @@
 
 set -eu -o pipefail
 
-# todo: make these customisable
-export GTFS_IMPORTER_VERBOSE=false
+export GTFS_POSTPROCESSING_D_PATH="${GTFS_POSTPROCESSING_D_PATH:-"$PWD/gtfs-postprocessing.d"}"
+export GTFS_DOWNLOAD_URL="${GTFS_DOWNLOAD_URL:-https://www.vbb.de/vbbgtfs}"
+export GTFS_DOWNLOAD_USER_AGENT="${GTFS_DOWNLOAD_USER_AGENT:-OpenDataVBB/gtfs-rt-metrics-server GTFS import}"
+export GTFS_IMPORTER_VERBOSE="${GTFS_IMPORTER_VERBOSE:-false}"
+export GTFS_IMPORTER_DB_PREFIX="${GTFS_IMPORTER_DB_PREFIX:-gtfs}"
+# The VBB GTFS feed usually doesn't need gtfstidy-ing.
+export GTFSTIDY_BEFORE_IMPORT="${GTFSTIDY_BEFORE_IMPORT:-false}"
 
 set -x
 
 # todo: pin version
 docker run \
 	--rm -it \
-	-v $PWD/gtfs:/var/gtfs \
-	-v $PWD/gtfs-postprocessing.d:/etc/gtfs/gtfs-postprocessing.d \
+	-v "$PWD/gtfs":/var/gtfs \
+	-v "$GTFS_POSTPROCESSING_D_PATH":/etc/gtfs/gtfs-postprocessing.d \
 	-v /tmp/gtfs:/tmp/gtfs \
 	-e GTFS_IMPORTER_VERBOSE \
 	-e GTFS_DOWNLOAD_USER_AGENT \
@@ -19,4 +24,5 @@ docker run \
 	-e GTFS_DOWNLOAD_URL \
 	-e GTFSTIDY_BEFORE_IMPORT \
 	-e GTFS_IMPORTER_DB_PREFIX \
+	-e GTFSTIDY_BEFORE_IMPORT -e GTFSTIDY_FIX_ZIP -e GTFSTIDY_DEFAULT_ON_ERRS -e GTFSTIDY_DROP_ERRS -e GTFSTIDY_CHECK_NULL_COORDS -e GTFSTIDY_MIN_SHAPES -e GTFSTIDY_MINIMIZE_SERVICES -e GTFSTIDY_MINIMIZE_STOPTIMES -e GTFSTIDY_DELETE_ORPHANS -e GTFSTIDY_REMOVE_REDUNDANT_AGENCIES -e GTFSTIDY_REMOVE_REDUNDANT_ROUTES -e GTFSTIDY_REMOVE_REDUNDANT_SERVICES -e GTFSTIDY_REMOVE_REDUNDANT_SHAPES -e GTFSTIDY_REMOVE_REDUNDANT_STOPS -e GTFSTIDY_REMOVE_REDUNDANT_TRIPS \
 	ghcr.io/opendatavbb/duckdb-gtfs-importer
