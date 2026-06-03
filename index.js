ba8c8c6833f78b0f90b6845a8f595844a8110d21
Jannis R <mail@jannisr.de>
2026-06-03 16:10:23 +0200

    suppress crashing on ECONNREFUSED fetch errors 🐛
    follow-up to 653261c

--- index.js
+++ index.js
@@ -448,6 +448,7 @@ const serveGtfsRtMetrics = async (cfg, opt = {}) => {
 				isProgrammerError(err)
 				// fetch errors
 				&& !(err instanceof TypeError && err.code === 'ENOENT')
+				&& !(err instanceof TypeError && err.code === 'ECONNREFUSED')
 				&& !(err instanceof TypeError && err.cause?.code === 'UND_ERR_CONNECT_TIMEOUT')
 				&& !(err instanceof TypeError && err.cause?.code === 'UND_ERR_BODY_TIMEOUT')
 			) {
